766a08075d7d85da016a02ea0650c55f9e3e583d
Armaan A <49625265+Armster15@users.noreply.github.com>
2022-08-03 01:05:43 -0700

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ const fileInput = document.querySelector("input#file-input") as HTMLInputElement
 fileInput.addEventListener("change", async () => {
   const file = fileInput.files![0];
   const buffer = new Uint8Array(await file.arrayBuffer());
-  const heic = heicDecode(buffer)
+  const heic = heicDecode({buffer: buffer})
   console.log(heic);
 })
 
