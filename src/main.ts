c71710029e0d546438701534831c022042135e73
Armaan A <armaanaggarwal@gmail.com>
2022-08-02 23:48:12 -0700

    first commit

---
+++ src/main.ts
@@ -0,0 +1,12 @@
+import heicDecode from "heic-decode";
+
+const fileInput = document.querySelector("input#file-input") as HTMLInputElement;
+
+fileInput.addEventListener("change", async () => {
+  const file = fileInput.files![0];
+  const buffer = new Uint8Array(await file.arrayBuffer());
+  const heic = heicDecode(buffer)
+  console.log(heic);
+})
+
+export {};
